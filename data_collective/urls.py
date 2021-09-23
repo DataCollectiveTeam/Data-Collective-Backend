b30924fdff66233a3b3c20dc7534c559ce38cc3d
dunnoconnor <moconnor8@gmail.com>
2021-09-23 11:37:21 -0400

    resolved url error

--- data_collective/urls.py
+++ data_collective/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('projects/', views.ProjectList.as_view(), name='project_list'),
-    path('projectss/<int:pk>', views.ProjectDetail.as_view(), name='project_detail'),
+    path('projects/<int:pk>', views.ProjectDetail.as_view(), name='project_detail'),
     path('citizens/', views.CitizenList.as_view(), name='citizen_list'),
     path('citizens/<int:pk>', views.CitizenDetail.as_view(), name='citizen_detail'),
     path('data_entries/', views.DataList.as_view(), name='data_list'),
